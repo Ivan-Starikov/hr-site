a9ef6573c7339b915c9e31b5d88a62cbc0ce1fd4
Ivan Starikov <ivan.starikov@flatstack.com>
2022-11-15 15:01:45 +0800

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'images/[hash][ext][query]',
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s?css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: ''}
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+    ],
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.css'],
+  }
+}
